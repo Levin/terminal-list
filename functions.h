223ae625ca07ca32de1084754ca1adbec1a01b87
levin <mail.an.levin@gmail.com>
2023-12-30 23:20:52 +0100

    work on display

--- functions.h
+++ functions.h
@@ -9,11 +9,21 @@ void send_login_greet()
 	time_t ti;
 	time(&ti);
 
-
 	printf("\n\n");
-	printf("\tWelcome, it's %s", ctime(&ti));
-	printf("\tYou are on a %u day streak, keep on going \n", get_streak());
-	printf("\tYou solved %u tasks this week", get_done());
+	printf("\t················································\n");
+	printf("\t:  __        __   _                            :\n");
+	printf("\t:  \\ \\      / /__| | ___ ___  _ __ ___   ___   :\n");
+	printf("\t:   \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\  :\n");
+	printf("\t:    \\ V  V /  __/ | (_| (_) | | | | | |  __/  :\n");
+	printf("\t:     \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  :\n");
+	printf("\t················································\n");
+	printf("\t\t\t\tit's %s", ctime(&ti));
+	printf("\t\t      ---\n");
+	printf("\tYou are on a | %u | day streak, keep on going \n", get_streak());
+	printf("\t\t      ---\n");
+	printf("\t\t    ---\n");
+	printf("\tYou solved | %u | tasks this week\n", get_done());
+	printf("\t\t    ---\n");
 	printf("\n\n");
 	printf("\tToday's Tasks: \n\n");
 	read_tasks();
@@ -21,6 +31,7 @@ void send_login_greet()
 	read_backlog();
 	printf("\n\n\tNot yet sheduled tasks: \n\n");
 	list_offlist();
+	printf("\n\n");
 
 }
 
